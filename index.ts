45137c990bf17877f7fedcbe9344796bb62d2851
Jackie <1121@generative.xyz>
2023-12-08 14:57:34 +0700

    fix: import aliases

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import "module-alias/register";
 import "dotenv/config";
 
 import app from "./src";
--- src/apis/v1/repositories/unitOfWork.ts
+++ src/apis/v1/repositories/unitOfWork.ts
@@ -1,4 +1,4 @@
-import { Pool } from "mysql2/promise";
+import { Pool, PoolConnection } from "mysql2/promise";
 import { IUserRepo } from "../repoInterfaces";
 import UserRepository from "./user.repository";
 
@@ -9,6 +9,23 @@ class UnitOfWork {
   constructor(pool: Pool) {
     this.pool = pool;
     this.userRepository = new UserRepository(pool);
+  }
+
+  async transaction<T>(callback: (connection: PoolConnection) => Promise<T>): Promise<T> {
+    const connection = await this.pool.getConnection();
+
+    try {
+      await connection.beginTransaction();
+      const result = await callback(connection);
+      await connection.commit();
+
+      return result;
+    } catch (error) {
+      await connection.rollback();
+      throw error;
+    } finally {
+      connection.release();
+    }
   }
 }
 
